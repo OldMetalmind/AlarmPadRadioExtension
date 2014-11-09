e4b466eeff1eb9534ee03969bf93ac8664a7a202
Filipe Emanuel dos Santos Barroso <filipe.barroso@gmail.com>
2014-11-09 04:19:04 +0000

    Update DirbleService.java
    Dirble API KEY removed

--- alarmpadradio/src/main/java/pt/abrrs/alarmpadradio/dirble/services/DirbleService.java
+++ alarmpadradio/src/main/java/pt/abrrs/alarmpadradio/dirble/services/DirbleService.java
@@ -9,7 +9,7 @@ import retrofit.http.Path;
 
 public interface DirbleService
 {
-    @GET("/country/apikey/9d07e8ac5393ede1c02f872a3ce97b928f6554dc/country/{country}")
+    @GET("/country/apikey/<Dirble API KEY>/country/{country}")
     void listRadiosByCountry(@Path("country") String country, Callback<List<Radios>> callback
     );
 }
